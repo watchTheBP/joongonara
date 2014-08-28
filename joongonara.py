34e065637d66a823fa0a874e0a549c9528e0027e
lucidus21 <lucidus21@gmail.com>
2014-08-28 19:10:58 +0900

    마지막 수정

--- joongonara.py
+++ joongonara.py
@@ -14,132 +14,144 @@ import time
 
 from elasticsearch import Elasticsearch
 
+
 def articleid_catcher():
-    cookie = {'Cookie':"ncvid=#vid#_210.57.236.92SzQw; WMONID=j2Y5SRWoyrC; NNB=WSMDQAO7Z73FG; nid_inf=2045810834; NID_AUT=8mmvoHcg+T1i063ormAnXPkE/8ZI+Vl1a4hQEgk6Akf3LELrYTyDQOw5XlHQV7WhGhjSBW1T6Y3+RR4IVzfUzGD8PCJyJePTo4nALeqURhYQSCNHnKTqiQQMkuQOs8Pv; NID_SES=AAABQ8yPygIgEO/Ihk9KOruk8asXNS28tcn+ictUEY4x6uHnI5Hz/yzHrM8pkNWh732JjaPAiCieMFO8kMU+QhaTAX4hLBHsP5KW7ReqvbFGSsYmP68G8vrlrOi7V39eI6UlDNY9npdjg4VQYSX6x+QunA/hVzMBSzxlsj4rHxb7I1r7FTsN5zJWwNhBqukn99lIcwSH4IAovsPF4HIG6aeplS4luC949udkN3zOftqknBOuunexz8FbyrrK2DGZ30w17mSR1rGGmfDRYcP1K6ZmhhNMah4CNFnU4U7bg0zW162GtNYpIafVaPLSJlX0csiLVFN9+v2MjhSpS0PewVwcOXt1fTP+qZmja+8DtpQJKwTtwRvamHbjFBo0SCgKxp4uFhhTIvTT71R/Ht7Kxyivx81sjMf6fA1yv7EC2sQG8NuuGnuD0FVzp8aU4cDHN8xR9w==; npic=DkmujVzw51p+goyLrc97euE9wHLh9fenNaf7Tn1byAN+pTaJokfC69TpJE/LwFyJCA==; ncu=89bb4b7e623b42fed6317e415a9ad0168f981988b80a; rmvc=10050146; ncvc2=1e7e86a2f6c0c27359a7ddeeca304b378fca3fa6fd9d43fe1fdae73488aa4c8752ae68b1744fddfe801a49e157d572ba384b7f63ce8b89a3904bf628f1dc66a3d2c4ccac82a88dd7c5c5c6c2c1c3d8dfc7d5cad5d3d5d5dbecebe89894b398aeb3b6bea3afbdaca3a3a5e9f6f0f1f78c8f8b8f98849a8581879b959e999e15; personaconmain|bernardjin=EAE11B32508CF3CEC41B74A48AE63E092C27FF2A39B7B9A218767A543203FB65; personacon|bernardjin=2ADD4716060F468BDB07BB69C1DC016B3FC4000403616BCC07C24AC6543D6F22; JSESSIONID=E13F1DCE6D8D4D3BD0812F2471F1CD6C; BMR="}
-    user_agent = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'}
+    cookie = {
+    'Cookie': "ncvid=#vid#_210.57.236.92SzQw; WMONID=j2Y5SRWoyrC; NNB=WSMDQAO7Z73FG; nid_inf=2045810834; NID_AUT=8mmvoHcg+T1i063ormAnXPkE/8ZI+Vl1a4hQEgk6Akf3LELrYTyDQOw5XlHQV7WhGhjSBW1T6Y3+RR4IVzfUzGD8PCJyJePTo4nALeqURhYQSCNHnKTqiQQMkuQOs8Pv; NID_SES=AAABQ8yPygIgEO/Ihk9KOruk8asXNS28tcn+ictUEY4x6uHnI5Hz/yzHrM8pkNWh732JjaPAiCieMFO8kMU+QhaTAX4hLBHsP5KW7ReqvbFGSsYmP68G8vrlrOi7V39eI6UlDNY9npdjg4VQYSX6x+QunA/hVzMBSzxlsj4rHxb7I1r7FTsN5zJWwNhBqukn99lIcwSH4IAovsPF4HIG6aeplS4luC949udkN3zOftqknBOuunexz8FbyrrK2DGZ30w17mSR1rGGmfDRYcP1K6ZmhhNMah4CNFnU4U7bg0zW162GtNYpIafVaPLSJlX0csiLVFN9+v2MjhSpS0PewVwcOXt1fTP+qZmja+8DtpQJKwTtwRvamHbjFBo0SCgKxp4uFhhTIvTT71R/Ht7Kxyivx81sjMf6fA1yv7EC2sQG8NuuGnuD0FVzp8aU4cDHN8xR9w==; npic=DkmujVzw51p+goyLrc97euE9wHLh9fenNaf7Tn1byAN+pTaJokfC69TpJE/LwFyJCA==; ncu=89bb4b7e623b42fed6317e415a9ad0168f981988b80a; rmvc=10050146; ncvc2=1e7e86a2f6c0c27359a7ddeeca304b378fca3fa6fd9d43fe1fdae73488aa4c8752ae68b1744fddfe801a49e157d572ba384b7f63ce8b89a3904bf628f1dc66a3d2c4ccac82a88dd7c5c5c6c2c1c3d8dfc7d5cad5d3d5d5dbecebe89894b398aeb3b6bea3afbdaca3a3a5e9f6f0f1f78c8f8b8f98849a8581879b959e999e15; personaconmain|bernardjin=EAE11B32508CF3CEC41B74A48AE63E092C27FF2A39B7B9A218767A543203FB65; personacon|bernardjin=2ADD4716060F468BDB07BB69C1DC016B3FC4000403616BCC07C24AC6543D6F22; JSESSIONID=E13F1DCE6D8D4D3BD0812F2471F1CD6C; BMR="}
+    user_agent = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'}
     url = "http://m.cafe.naver.com/joonggonara"
     soup = BeautifulSoup(requests.get(url, cookies=cookie, headers=user_agent).text)
-    root = soup.findAll('ul', {'class':'lst4'})
+    root = soup.findAll('ul', {'class': 'lst4'})
     for link in root:
         a = link.find('a')['href']
         a = a.split("articleid=")[1].split("&")[0]
-    file = open('joongonara_log.txt', 'w')
-    file.write("articleid : " + a)
-    file.close()
+
+    return int(a)  # recent article id
+
+    # file = open('joongonara_log.txt', 'w')
+    # file.write("articleid : " + a)
+    # file.close()
 
 
 def main():
     joongonara_index = dict()
     joongonara = dict()
-    cookie = {'Cookie':"ncvid=#vid#_210.57.236.92SzQw; WMONID=j2Y5SRWoyrC; NNB=WSMDQAO7Z73FG; nid_inf=2045810834; NID_AUT=8mmvoHcg+T1i063ormAnXPkE/8ZI+Vl1a4hQEgk6Akf3LELrYTyDQOw5XlHQV7WhGhjSBW1T6Y3+RR4IVzfUzGD8PCJyJePTo4nALeqURhYQSCNHnKTqiQQMkuQOs8Pv; NID_SES=AAABQ8yPygIgEO/Ihk9KOruk8asXNS28tcn+ictUEY4x6uHnI5Hz/yzHrM8pkNWh732JjaPAiCieMFO8kMU+QhaTAX4hLBHsP5KW7ReqvbFGSsYmP68G8vrlrOi7V39eI6UlDNY9npdjg4VQYSX6x+QunA/hVzMBSzxlsj4rHxb7I1r7FTsN5zJWwNhBqukn99lIcwSH4IAovsPF4HIG6aeplS4luC949udkN3zOftqknBOuunexz8FbyrrK2DGZ30w17mSR1rGGmfDRYcP1K6ZmhhNMah4CNFnU4U7bg0zW162GtNYpIafVaPLSJlX0csiLVFN9+v2MjhSpS0PewVwcOXt1fTP+qZmja+8DtpQJKwTtwRvamHbjFBo0SCgKxp4uFhhTIvTT71R/Ht7Kxyivx81sjMf6fA1yv7EC2sQG8NuuGnuD0FVzp8aU4cDHN8xR9w==; npic=DkmujVzw51p+goyLrc97euE9wHLh9fenNaf7Tn1byAN+pTaJokfC69TpJE/LwFyJCA==; ncu=89bb4b7e623b42fed6317e415a9ad0168f981988b80a; rmvc=10050146; ncvc2=1e7e86a2f6c0c27359a7ddeeca304b378fca3fa6fd9d43fe1fdae73488aa4c8752ae68b1744fddfe801a49e157d572ba384b7f63ce8b89a3904bf628f1dc66a3d2c4ccac82a88dd7c5c5c6c2c1c3d8dfc7d5cad5d3d5d5dbecebe89894b398aeb3b6bea3afbdaca3a3a5e9f6f0f1f78c8f8b8f98849a8581879b959e999e15; personaconmain|bernardjin=EAE11B32508CF3CEC41B74A48AE63E092C27FF2A39B7B9A218767A543203FB65; personacon|bernardjin=2ADD4716060F468BDB07BB69C1DC016B3FC4000403616BCC07C24AC6543D6F22; JSESSIONID=E13F1DCE6D8D4D3BD0812F2471F1CD6C; BMR="}
-    user_agent = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'}
+    cookie = {
+    'Cookie': "ncvid=#vid#_210.57.236.92SzQw; WMONID=j2Y5SRWoyrC; NNB=WSMDQAO7Z73FG; nid_inf=2045810834; NID_AUT=8mmvoHcg+T1i063ormAnXPkE/8ZI+Vl1a4hQEgk6Akf3LELrYTyDQOw5XlHQV7WhGhjSBW1T6Y3+RR4IVzfUzGD8PCJyJePTo4nALeqURhYQSCNHnKTqiQQMkuQOs8Pv; NID_SES=AAABQ8yPygIgEO/Ihk9KOruk8asXNS28tcn+ictUEY4x6uHnI5Hz/yzHrM8pkNWh732JjaPAiCieMFO8kMU+QhaTAX4hLBHsP5KW7ReqvbFGSsYmP68G8vrlrOi7V39eI6UlDNY9npdjg4VQYSX6x+QunA/hVzMBSzxlsj4rHxb7I1r7FTsN5zJWwNhBqukn99lIcwSH4IAovsPF4HIG6aeplS4luC949udkN3zOftqknBOuunexz8FbyrrK2DGZ30w17mSR1rGGmfDRYcP1K6ZmhhNMah4CNFnU4U7bg0zW162GtNYpIafVaPLSJlX0csiLVFN9+v2MjhSpS0PewVwcOXt1fTP+qZmja+8DtpQJKwTtwRvamHbjFBo0SCgKxp4uFhhTIvTT71R/Ht7Kxyivx81sjMf6fA1yv7EC2sQG8NuuGnuD0FVzp8aU4cDHN8xR9w==; npic=DkmujVzw51p+goyLrc97euE9wHLh9fenNaf7Tn1byAN+pTaJokfC69TpJE/LwFyJCA==; ncu=89bb4b7e623b42fed6317e415a9ad0168f981988b80a; rmvc=10050146; ncvc2=1e7e86a2f6c0c27359a7ddeeca304b378fca3fa6fd9d43fe1fdae73488aa4c8752ae68b1744fddfe801a49e157d572ba384b7f63ce8b89a3904bf628f1dc66a3d2c4ccac82a88dd7c5c5c6c2c1c3d8dfc7d5cad5d3d5d5dbecebe89894b398aeb3b6bea3afbdaca3a3a5e9f6f0f1f78c8f8b8f98849a8581879b959e999e15; personaconmain|bernardjin=EAE11B32508CF3CEC41B74A48AE63E092C27FF2A39B7B9A218767A543203FB65; personacon|bernardjin=2ADD4716060F468BDB07BB69C1DC016B3FC4000403616BCC07C24AC6543D6F22; JSESSIONID=E13F1DCE6D8D4D3BD0812F2471F1CD6C; BMR="}
+    user_agent = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'}
     file = open('joongonara_log.txt', 'r')
-    a = int(file.read().split(":")[1].strip())
-    with open('joongonara.txt', 'w') as outfile:
-        crawl_time = time.strftime("%Y/%m/%d %H:%M:%S")
-        joongonara.update({"crawl_time": crawl_time})
-        for i in range(a - 25000, a):
-            try:
-                url = "http://m.cafe.naver.com/ArticleRead.nhn?clubid=10050146&articleid={0}&page=1&boardtype=L".format(i)
-                page = requests.get(url, cookies=cookie, headers=user_agent)
-                tree = html.fromstring(page.text)
+    start_point = int(file.read().split(":")[1].strip())
+    file.close()
 
-                if tree.xpath( ('//*[@id="ct"]/p/text()').encode('utf-8') ) != [u'\uac8c\uc2dc\ubb3c\uc774 \uc874\uc7ac\ud558\uc9c0 \uc54a\uac70\ub098 \uc0ad\uc81c\ub418\uc5c8\uc2b5\ub2c8\ub2e4.']:
+    # with open('joongonara.txt', 'w') as outfile:
+    crawl_time = time.strftime("%Y/%m/%d %H:%M:%S")
+    joongonara.update({"crawl_time": crawl_time})
+    for i in range(start_point, articleid_catcher()):
+        try:
+            url = "http://m.cafe.naver.com/ArticleRead.nhn?clubid=10050146&articleid={0}&page=1&boardtype=L".format(i)
+            page = requests.get(url, cookies=cookie, headers=user_agent)
+            tree = html.fromstring(page.text)
 
-                    if tree.xpath('//*[@id="ct"]/div[3]/div[1]/span[1]/text()') != []:
+            if tree.xpath(('//*[@id="ct"]/p/text()').encode('utf-8')) != [
+                u'\uac8c\uc2dc\ubb3c\uc774 \uc874\uc7ac\ud558\uc9c0 \uc54a\uac70\ub098 \uc0ad\uc81c\ub418\uc5c8\uc2b5\ub2c8\ub2e4.']:
 
-                        if ( tree.xpath('//*[@id="ct"]/div[3]/div[1]/span[1]/text()')[0] ).encode('utf-8') == "판매중":
+                if tree.xpath('//*[@id="ct"]/div[3]/div[1]/span[1]/text()') != []:
 
-                            if tree.xpath('//*[@id="ct"]/p') != None:
+                    if ( tree.xpath('//*[@id="ct"]/div[3]/div[1]/span[1]/text()')[0] ).encode('utf-8') == "판매중":
 
-                                #title
-                                #article_id
-                                if tree.xpath('//*[@id="ct"]/div[1]/h2/text()')[0].strip() != None:
-                                    title = tree.xpath('//*[@id="ct"]/div[1]/h2/text()')[0].strip()
-                                    title = title.encode('utf-8')
-                                    joongonara.update({"title":title})
-                                    joongonara.update({"article_id":i})
-                                    joongonara_index.update({"index":{"_index":"joongo","_type":"text"}})
+                        if tree.xpath('//*[@id="ct"]/p') != None:
 
-                                #category
-                                if tree.xpath('//*[@id="ct"]/div[1]/p/span/span[1]/a/text()')[0].strip() != None:
+                            #title
+                            #article_id
+                            if tree.xpath('//*[@id="ct"]/div[1]/h2/text()')[0].strip() != None:
+                                title = tree.xpath('//*[@id="ct"]/div[1]/h2/text()')[0].strip()
+                                title = title.encode('utf-8')
+                                joongonara.update({"title": title})
+                                joongonara.update({"article_id": i})
+                                joongonara_index.update({"index": {"_index": "joongo", "_type": "text"}})
 
-                                    cat = tree.xpath('//*[@id="ct"]/div[1]/p/span/span[1]/a/text()')[0].strip()
-                                    if isinstance(cat, unicode) == True:
-                                        cat_link = "//a[text()='" + cat + "']/@href"
-                                        category = tree.xpath(cat_link)[0]
-                                        category = str(category)
-                                        url2 = "http://m.cafe.naver.com" + category
-                                        page2 = requests.get(url2, cookies=cookie, headers=user_agent)
-                                        tree2 = html.fromstring(page2.text)
-                                        category = tree2.xpath('//*[@id="ch"]/div[3]/h2/text()')[1].strip()
-                                        category = category.encode('utf-8')
-                                        joongonara.update({"category":category})
+                            #category
+                            if tree.xpath('//*[@id="ct"]/div[1]/p/span/span[1]/a/text()')[0].strip() != None:
 
-                                #views
-                                if tree.xpath('//*[@id="ct"]/div[1]/p/span/span[2]/text()')[1].strip().split()[1] != None:
-                                    views = tree.xpath('//*[@id="ct"]/div[1]/p/span/span[2]/text()')[1].strip().split()[1]
-                                    v = views.encode('utf-8')
-                                    views = int(re.match(r'\d+', v).group())
-                                    joongonara.update({"views":views})
+                                cat = tree.xpath('//*[@id="ct"]/div[1]/p/span/span[1]/a/text()')[0].strip()
+                                if isinstance(cat, unicode) == True:
+                                    cat_link = "//a[text()='" + cat + "']/@href"
+                                    category = tree.xpath(cat_link)[0]
+                                    category = str(category)
+                                    url2 = "http://m.cafe.naver.com" + category
+                                    page2 = requests.get(url2, cookies=cookie, headers=user_agent)
+                                    tree2 = html.fromstring(page2.text)
+                                    category = tree2.xpath('//*[@id="ch"]/div[3]/h2/text()')[1].strip()
+                                    category = category.encode('utf-8')
+                                    joongonara.update({"category": category})
 
-                                #date
-                                if tree.xpath('//*[@id="ct"]/div[1]/p/span/span[1]/text()')[2].strip() != None:
-                                    date = tree.xpath('//*[@id="ct"]/div[1]/p/span/span[1]/text()')[2].strip()
-                                    date_time = datetime.strptime(date, '%Y.%m.%d %H:%M')
-                                    date_time = str(date_time)
-                                    joongonara.update({"datetime":date_time})
+                            #views
+                            if tree.xpath('//*[@id="ct"]/div[1]/p/span/span[2]/text()')[1].strip().split()[1] != None:
+                                views = tree.xpath('//*[@id="ct"]/div[1]/p/span/span[2]/text()')[1].strip().split()[1]
+                                v = views.encode('utf-8')
+                                views = int(re.match(r'\d+', v).group())
+                                joongonara.update({"views": views})
 
-                                #comments
-                                if tree.xpath('//*[@id="ct"]/div[1]/a/text()') != None:
-                                    comments = tree.xpath('//*[@id="ct"]/div[1]/a/text()')[1].strip()
-                                    c = comments.encode('utf-8')
-                                    c = int(c)
-                                    joongonara.update({"comments":comments})
+                            #date
+                            if tree.xpath('//*[@id="ct"]/div[1]/p/span/span[1]/text()')[2].strip() != None:
+                                date = tree.xpath('//*[@id="ct"]/div[1]/p/span/span[1]/text()')[2].strip()
+                                date_time = datetime.strptime(date, '%Y.%m.%d %H:%M')
+                                date_time = str(date_time)
+                                joongonara.update({"datetime": date_time})
 
-                                #phone
-                                if tree.xpath('//*[@id="phoneNumber"]/text()'):
-                                    phone = tree.xpath('//*[@id="phoneNumber"]/text()')[0]
-                                    joongonara.update({"phone":phone})
+                            #comments
+                            if tree.xpath('//*[@id="ct"]/div[1]/a/text()') != None:
+                                comments = tree.xpath('//*[@id="ct"]/div[1]/a/text()')[1].strip()
+                                c = comments.encode('utf-8')
+                                c = int(c)
+                                joongonara.update({"comments": comments})
 
-                                #body
-                                body = tree.xpath('//*[@id="postContent"]')[0]
-                                body = body.text_content().strip()
-                                body = body.encode('utf-8')
-                                joongonara.update({"body":body})
+                            #phone
+                            if tree.xpath('//*[@id="phoneNumber"]/text()'):
+                                phone = tree.xpath('//*[@id="phoneNumber"]/text()')[0]
+                                joongonara.update({"phone": phone})
 
-                                #email
-                                if tree.xpath('//*[@id="seller_info"]/div/ul/li[2]/p/a/text()') != None:
-                                    email = tree.xpath('//*[@id="seller_info"]/div/ul/li[2]/p/a/text()')
-                                    if email == []:
-                                        email = "None"
-                                    else:
-                                        email = email[0].strip()
+                            #body
+                            body = tree.xpath('//*[@id="postContent"]')[0]
+                            body = body.text_content().strip()
+                            body = body.encode('utf-8')
+                            joongonara.update({"body": body})
 
-                                    joongonara.update({"email":email})
-#                                    print json.dumps(joongonara_index)
-#                                    print json.dumps(joongonara, ensure_ascii=False)
-                                print "article_id," + str(joongonara['article_id'])
-                                es = Elasticsearch()
+                            #email
+                            if tree.xpath('//*[@id="seller_info"]/div/ul/li[2]/p/a/text()') != None:
+                                email = tree.xpath('//*[@id="seller_info"]/div/ul/li[2]/p/a/text()')
+                                if email == []:
+                                    email = "None"
+                                else:
+                                    email = email[0].strip()
 
-                                doc = joongonara
-                                doc['datetime'] = datetime.strptime(doc['datetime'], "%Y-%m-%d %H:%M:%S")
-                                doc['crawl_time'] = datetime.strptime(doc['crawl_time'], "%Y/%m/%d %H:%M:%S")
+                                joongonara.update({"email": email})
+                            #                                    print json.dumps(joongonara_index)
+                            #                                    print json.dumps(joongonara, ensure_ascii=False)
+                            print "article_id," + str(joongonara['article_id'])
+                            es = Elasticsearch()
 
-                                res = es.index(index="joongonara", doc_type="post", body=doc) # no id
-                                print res['created']
+                            doc = joongonara
+                            doc['datetime'] = datetime.strptime(doc['datetime'], "%Y-%m-%d %H:%M:%S")
+                            doc['crawl_time'] = datetime.strptime(doc['crawl_time'], "%Y/%m/%d %H:%M:%S")
 
-                                #json.dump(joongonara_index, outfile)
-                                #json.dump(joongonara, outfile, ensure_ascii=False)
-                                sleep(random.randint(1,3))
-                            else:
-                                break
-            except ConnectionError:
-                pass
-        file.close()
-        file = open('joongonara_log.txt', 'w')
-        id = str(a + 25000)
-        file.write("articleid : " + id)
+                            res = es.index(index="joongonara", doc_type="post", body=doc)  # no id
+                            print res['created']
+
+                            #json.dump(joongonara_index, outfile)
+                            #json.dump(joongonara, outfile, ensure_ascii=False)
+                            sleep(random.randint(1, 3))
+                        else:
+                            break
+            update_last_crawl_articleid = open('joongonara_log.txt', 'w')
+            update_last_crawl_articleid.write("articleid : " + i)
+            update_last_crawl_articleid.close()
+        except ConnectionError:
+            pass
+
+
 
 if __name__ == '__main__':
     main()
